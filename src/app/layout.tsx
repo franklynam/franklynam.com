06778ed1572adccbe171418a4da1d20acae1c658
Frank Lynam <frank@franklynam.com>
2025-07-26 08:47:16 +0100

    Removed some unused assets | updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,23 @@ import Footer from "@/components/Footer";
 export const metadata: Metadata = {
   title: "franklynam.com",
   description: "software engineering as craft",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+    other: [
+      {
+        rel: "icon",
+        url: "/android-chrome-192x192.png",
+      },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
